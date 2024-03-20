dbf3e3e39ad9d696f9208f2aac236df69c79c7ac
Vivek Kumar <78255968+vinluvs@users.noreply.github.com>
2024-03-20 20:56:21 +0530

    resolved unit tests

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue= 0.875;
+        double expected_miles=cashValue/0.0035;
+        var rewardValue =new RewardValue(cashValue);
+        assertEquals(expected_miles, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue= 250;
+        double expected_cash=milesValue*0.0035;
+        var rewardValue =new RewardValue(milesValue);
+        assertEquals(expected_cash, rewardValue.getCashValue());
     }
 }
