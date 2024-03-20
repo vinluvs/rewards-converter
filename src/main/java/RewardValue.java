1e5549719fa737afe8fe4a46c38bf080fd3a0e8e
Vivek Kumar <78255968+vinluvs@users.noreply.github.com>
2024-03-20 19:56:50 +0530

    Add RewardsValue class to convert cash to miles

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,24 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+    private static final double Conversion_rate=0.0035;
+    public RewardValue(double cash)
+    {
+        this.cashValue=cash;
+        this.milesValue=cash/Conversion_rate;
+    }
+    public RewardValue( int miles)
+    {
+        this.milesValue=miles;
+        this.cashValue=miles/Conversion_rate;
+    }
+
+    public double getCashValue()
+    {
+        return this.cashValue;
+    }
+    public double getMilesValue()
+    {
+        return this.milesValue;
+    }
+}
